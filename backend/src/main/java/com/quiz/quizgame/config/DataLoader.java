d76cde5bf40c62593a3748e05d8c897e8bfef1bd
Mohamed_Konate <mohamedkonatedevelopper@gmail.com>
2026-03-04 15:26:20 +0300

    modified

--- backend/src/main/java/com/quiz/quizgame/config/DataLoader.java
+++ backend/src/main/java/com/quiz/quizgame/config/DataLoader.java
@@ -17,39 +17,21 @@ public class DataLoader
 	}
 
 	@PostConstruct
-	public void loadData ()
+	public void init ()
 	{
 
-		// 🔥 Si des questions existent → on ne fait rien
 		if ( quizRepository.count () > 0 )
 		{
-			System.out.println ( "📦 Questions already exist — skipping data load." );
+			System.out.println ( "📦 Questions already exist." );
 			return;
 		}
 
-		System.out.println ( "🚀 Inserting default quiz questions..." );
+		System.out.println ( "🚀 Inserting quiz questions..." );
 
-		Quiz q1 = new Quiz ();
-		q1.setQuestion ( "2 + 2 ?" );
-		q1.setOption_1 ( "3" );
-		q1.setOption_2 ( "4" );
-		q1.setOption_3 ( "5" );
-		q1.setOption_4 ( "6" );
-		q1.setImage_url ( "images/france.jpg" );
-		q1.setAnswer ( "4" );
+		quizRepository.save ( new Quiz ( 0 , "2 + 2 ?" , "3" , "4" , "5" , "6" , "images/france.jpg" , "4" ) );
 
-		Quiz q2 = new Quiz ();
-		q2.setQuestion ( "Capital of France ?" );
-		q2.setOption_1 ( "Berlin" );
-		q2.setOption_2 ( "Madrid" );
-		q2.setOption_3 ( "Paris" );
-		q2.setOption_4 ( "Rome" );
-		q1.setImage_url ( "images/france.jpg" );
-		q2.setAnswer ( "Paris" );
+		quizRepository.save ( new Quiz ( 0 , "Capital of France ?" , "Berlin" , "Madrid" , "Paris" , "Rome" , "images/france.jpg" , "Paris" ) );
 
-		quizRepository.save ( q1 );
-		quizRepository.save ( q2 );
-
-		System.out.println ( "✅ Default questions inserted." );
+		System.out.println ( "✅ Questions inserted." );
 	}
 }
